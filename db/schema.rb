5b19761f25122300236e7622640233c3abb0c022
Xavier Everaert <xeveraert@gmail.com>
2020-08-20 12:46:15 +0200

    added reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_045143) do
+ActiveRecord::Schema.define(version: 2020_08_17_055319) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2020_08_17_045143) do
     t.string "chef"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
